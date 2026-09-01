b9404d86c3be556503ab5ab09b69abf0af079be1
SamiraMartinsT <samiramartins996@gmail.com>
2026-09-01 19:38:45 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+Scanner input= new Scanner (System.in);
+int A;
+int B;
+int NA;
+int NB;
+
+    System.out.println("Digite o  valor de A:");
+    A = input.nextInt();
+
+    System.out.println("Digite o valor de B:");
+    B = input.nextInt();
+
+    NA = B;
+    NB = A;
+    System.out.println("O valor de A é "+ " "+ NA + " " + " e o valor de B é "+" "+ NB);
+}
