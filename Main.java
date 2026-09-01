a546bdff1ecbaac58da56e1f390611eea65d6a32
SamiraMartinsT <samiramartins996@gmail.com>
2026-09-01 19:24:30 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+Scanner input = new Scanner (System.in);
+double N1;
+double dobro;
+    System.out.println("Digite um número:");
+    N1 = input.nextDouble();
+    dobro = N1*2;
+    System.out.println("O dobro do número digitado é:"+ ""+ dobro);
+
+}
