f11b6862e8b495de781213b4ba98b36ab24e9e18
Samira Martins <samiramartins996@gmail.com>
2026-09-22 19:39:23 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    for (int i = 10; i>= 1; i--) {
+        System.out.println("Contagem = " + i);
+    }
+    System.out.println("DECOLAR!");
+}
