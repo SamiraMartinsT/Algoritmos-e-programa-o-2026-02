c13a5b01f61dabe9c442ccbf08b80c347e1fef4b
Samira Martins <samiramartins996@gmail.com>
2026-09-22 19:37:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    /*for ( int i= 0; i < 50; i++) {
+        System.out.println("Valor = " + i);
+    }*/
+
+    int i=1;
+    while (i <= 10){
+        System.out.println("Valor =" + i);
+        i++;
+    }
+}
