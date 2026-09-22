48bd7957767d3d8c1a523e2c246aacf9873f28a8
Samira Martins <samiramartins996@gmail.com>
2026-09-22 19:47:27 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    for (int i=1; i<=20; i+=2)
+    System.out.println("Contagem de 2 em 2: "+ i);
+}
